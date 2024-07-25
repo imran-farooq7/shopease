f18b670a8098ac03aa29fb8d73bcca7d38aa1e34
Imran Farooq <imran_farooq7@hotmail.com>
2024-07-26 01:56:23 +0500

    Checkout functionality added

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,21 +1,26 @@
 import { auth } from "@/auth";
+import prisma from "@/prisma/client";
 import { cartItem } from "@/store/store";
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 	apiVersion: "2024-06-20",
 });
+
 const orderTotalAmount = (items: cartItem[]) => {
-	const totalAmount = items.reduce(
+	const totalAmount = items?.reduce(
 		(acc, item) => acc + item.price * item.quantity!,
 		0
 	);
 	return totalAmount;
 };
-export async function POST(req: NextApiRequest) {
+
+export async function POST(req: NextRequest) {
 	const session = await auth();
-	const { cart, payment_intent_id } = req.body;
+	const body = await req.json();
+	const { cart, payment_intent_id } = body;
+
 	if (!session?.user) {
 		return NextResponse.json(
 			{ message: "Not logged in" },
@@ -24,20 +29,92 @@ export async function POST(req: NextApiRequest) {
 			}
 		);
 	}
+
 	const orderData = {
-		user: { connect: session.user.id },
-		totalAmount: orderTotalAmount(cart),
+		userId: session.user.id!,
+		amount: orderTotalAmount(cart),
 		currency: "usd",
 		status: "pending",
 		paymentIntentId: payment_intent_id,
 		products: {
-			create: cart.map((item: cartItem) => ({
+			create: cart?.map((item: any) => ({
 				name: item.name,
 				description: item.description,
-				price: item.price,
+				price: parseFloat(item.price),
 				quantity: item.quantity,
+				image: item.image,
 			})),
 		},
 	};
-	return NextResponse.json(session.user, { status: 200 });
+
+	try {
+		if (payment_intent_id) {
+			console.log("Existing payment intent:", payment_intent_id);
+			const currentIntent = await stripe.paymentIntents.retrieve(
+				payment_intent_id
+			);
+			if (currentIntent) {
+				const updatePaymentIntent = await stripe.paymentIntents.update(
+					payment_intent_id,
+					{
+						amount: orderTotalAmount(cart) * 100,
+					}
+				);
+				const existingOrder = await prisma.order.findFirst({
+					where: { paymentIntentId: updatePaymentIntent.id },
+					include: { products: true },
+				});
+				if (!existingOrder) {
+					console.log("Invalid payment intent");
+					return NextResponse.json(
+						{ message: "Invalid payment intent" },
+						{ status: 400 }
+					);
+				}
+				const updatedOrder = await prisma.order.update({
+					where: { id: existingOrder.id },
+					data: {
+						amount: orderTotalAmount(cart),
+						products: {
+							deleteMany: {},
+							create: cart.map((item: any) => ({
+								name: item.name,
+								description: item.description,
+								price: parseFloat(item.price),
+								quantity: item.quantity,
+								image: item.image,
+							})),
+						},
+					},
+				});
+				return NextResponse.json(
+					{ paymentIntent: updatePaymentIntent },
+					{ status: 200 }
+				);
+			}
+		} else {
+			console.log("Creating new payment intent");
+			const paymentIntent = await stripe.paymentIntents.create({
+				amount: orderTotalAmount(cart) * 100,
+				currency: "usd",
+				automatic_payment_methods: { enabled: true },
+			});
+			const newOrder = await prisma.order.create({
+				data: {
+					amount: orderData.amount,
+					paymentIntentId: paymentIntent.id,
+					status: orderData.status,
+					userId: orderData.userId,
+					products: orderData.products,
+				},
+			});
+			return NextResponse.json({ paymentIntent }, { status: 200 });
+		}
+	} catch (error) {
+		console.error("Error processing request:", error);
+		return NextResponse.json(
+			{ message: "Internal server error" },
+			{ status: 500 }
+		);
+	}
 }
--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { useCartState } from "@/store/store";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY as string);
@@ -12,27 +10,33 @@ const Checkout = () => {
 	const cartState = useCartState();
 	const router = useRouter();
 	const [clientSecret, setClientSecret] = useState("");
+
+	const createCustomerPaymentIntent = async () => {
+		console.log("Creating customer payment intent");
+		const res = await fetch("/api/create-payment-intent", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				cart: cartState.cart,
+				payment_intent_id: cartState.paymentIntent,
+			}),
+		});
+		if (res.status === 403) {
+			router.push("/api/auth/signin");
+		}
+		const data = await res.json();
+		setClientSecret(data.paymentIntent.client_secret);
+		cartState.setPaymentIntent(data.paymentIntent.id);
+	};
+	console.log(clientSecret, "client secret");
+
 	useEffect(() => {
-		const createCustomerPaymentIntent = async () => {
-			const res = await fetch("/api/create-payment-intent", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					cart: cartState.cart,
-					payment_intent_id: cartState.paymentIntent,
-				}),
-			});
-			if (res.status === 403) {
-				router.push("/api/auth/signin");
-			}
-			const data = await res.json();
-			return data;
-		};
 		createCustomerPaymentIntent();
 	}, []);
 
 	return <div>Checkout</div>;
 };
-export default Checkout;
+
+export default React.memo(Checkout);
